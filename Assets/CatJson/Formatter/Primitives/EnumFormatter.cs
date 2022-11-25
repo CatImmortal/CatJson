011c54a9928961f01d235c4f09aa2524d0f6d095
wanger <18530117331@163.com>
2022-11-26 01:06:06 +0800

    增加int枚举支持

--- Assets/CatJson/Formatter/Primitives/EnumFormatter.cs
+++ Assets/CatJson/Formatter/Primitives/EnumFormatter.cs
@@ -18,8 +18,18 @@ namespace CatJson
         /// <inheritdoc />
         public object ParseJson(JsonParser parser, Type type, Type realType)
         {
-            RangeString rs = parser.Lexer.GetNextTokenByType(TokenType.String);
-            object enumOBj = Enum.Parse(realType,rs.ToString());
+            object enumOBj = null;
+            var nextTokenType = parser.Lexer.LookNextTokenType();
+            if (nextTokenType == TokenType.String)
+            {
+                RangeString rs = parser.Lexer.GetNextTokenByType(TokenType.String);
+                enumOBj = Enum.Parse(realType, rs.ToString());
+            }
+            else if (nextTokenType == TokenType.Number)
+            {
+                RangeString rs = parser.Lexer.GetNextTokenByType(TokenType.Number);
+                enumOBj = Enum.ToObject(realType, rs.AsInt());
+            }
             return enumOBj;
         }
     }
